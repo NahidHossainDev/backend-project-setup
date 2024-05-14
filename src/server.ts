670a07a34ee18822b8302b77cea5ae2ae750a3b5
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-05-14 22:41:17 +0600

    server grasfully closed for unhandleRejection

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,53 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config'
-import { errorLogger, logger } from './shared/logger'
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { errorLogger, logger } from './shared/logger';
+
+process.on('uncaughtException', error => {
+  errorLogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info('Mongo server connected!')
+    await mongoose.connect(config.database_url as string);
+    logger.info('Mongo server connected!');
 
-    app.listen(config.port, () => {
-      logger.info(`Server listening on port ${config.port}`)
-    })
+    server = app.listen(config.port, () => {
+      logger.info(`Server listening on port ${config.port}`);
+    });
   } catch (error) {
-    errorLogger.error('Failed to connect with DB.' + error)
+    errorLogger.error('Failed to connect with DB.' + error);
   }
+
+  process.on('unhandledRejection', async error => {
+    errorLogger.error('Server is shutting down for unhandle rejection!');
+
+    if (server) {
+      try {
+        server.close(err => {
+          if (err) errorLogger.error(err);
+          else errorLogger.error(error);
+          process.exit(1);
+        });
+      } catch (err) {
+        errorLogger.error(err);
+      }
+    } else {
+      process.exit(1);
+    }
+  });
 }
 
-bootstrap()
+bootstrap();
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is closed');
+  if (server) {
+    server.close();
+  }
+  process.exit(1);
+});
